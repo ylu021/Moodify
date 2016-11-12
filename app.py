432a946bd14cc8c7aefa8de027275ba241113553
Ubuntu <moodifty@Moodifty.zwtcm52azyge3f5qkqr4rmxqgc.bx.internal.cloudapp.net>
2016-11-12 15:07:47 +0000

    file upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def upload_file():
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-		return redirect(url_for('uploaded_file', filename=filename))
+		return redirect(url_for('upload_file', filename=filename))
 	return "ERROR"
 	
 # @app.route("/uploads/<filename>")
