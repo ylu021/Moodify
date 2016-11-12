e9ebc2379ad8f067e6cb58c50d78b61c576bff9e
ylu021 <yichen_0@hotmail.com>
2016-11-12 10:02:44 -0500

    add uploads

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'uploads/')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -39,11 +39,11 @@ def upload_file():
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-		return redirect(url_for('uploaded_file', filename=filename))
+		return redirect(url_for('upload_file', filename=filename))
 	
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+# @app.route("/uploads/<filename>")
+# def uploaded_file(filename):
+# 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
