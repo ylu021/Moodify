fcdda53f4dcc5a29c3ffab8e7891b4a098dca6ae
Ubuntu <moodifty@Moodifty.zwtcm52azyge3f5qkqr4rmxqgc.bx.internal.cloudapp.net>
2016-11-13 04:36:33 +0000

    upload image

--- moodnode/index.js
+++ moodnode/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var multer = require('multer');
 var app = express();
+var bodyParser = require('body-parser');
 
 console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+var upload = multer({ dest : 'uploads/' });
+
+
+app.post('/uploads',upload.array('userPhoto',10), function(req,res){
+        console.log(req.body);
+        console.log(req.files);
+        res.end("File is uploaded");
+});
+
+//app.post('/uploads', function(req, res) {
+//    res.send('Photo array?: ' + req.body.userPhoto);
+//});
 
 app.listen(8000, function(){
         console.log('Listening at port 8000');
