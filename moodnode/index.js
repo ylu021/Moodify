fc035145b9058f055f2caaaf15a640a33e4b8d43
Ubuntu <moodifty@Moodifty.zwtcm52azyge3f5qkqr4rmxqgc.bx.internal.cloudapp.net>
2016-11-13 03:08:19 +0000

    removing node_modules

--- moodnode/index.js
+++ moodnode/index.js
@@ -1,29 +1,11 @@
 var express = require('express');
 var multer = require('multer');
 var app = express();
-var storage = multer.diskStorage({
-        destionation: function(req,file,callback){
-                callback(null,'./uploads');
-        },
-        filename: function(req,file,callback){
-                callback(null, file.fieldname + '-' + Date.now());
-        }
-});
-var upload = multer({storage:storage}).single('userPhoto');
 
-app.get('/',function(req,res) {
-        res.send(__dirname + "/index.html");
+console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8000, function(){
+        console.log('Listening at port 8000');
 });
 
-app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
-
-app.listen(8888,function(){
-	console.log("runnning moodnode on port 8888");
-});
